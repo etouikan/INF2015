682d680bdf4f7d668d0180c2f7679036417221f6
elliotttouikan <elliotttouikan@172.29.114.220>
2016-01-14 19:24:07 -0500

    Commit initial

--- src/inf2015/INF2015.java
+++ src/inf2015/INF2015.java
@@ -15,7 +15,7 @@ public class INF2015 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
         // TODO code application logic here
     }
     
